20d8b5230cde247a7fd4b5d9af1e132bdb7d6601
Sawich <savich20022@gmail.com>
2024-03-20 17:51:05 +0800

    5com

--- MyBot/Bot.py
+++ MyBot/Bot.py
@@ -39,18 +39,18 @@ def menu (call):
         btn1 = types.InlineKeyboardButton(text='1. Пропустить пару', callback_data="v1")
         btn2 = types.InlineKeyboardButton(text='2. Ели как собраться и пойти', callback_data="v2")
         markup.add(btn1, btn2)
-        etap1 = open('text/1etap.txt', 'rb')
-        bot.send_message(call.message.chat.id, etap1)
-        img1 = open('img/etap1.png', 'rb')
-        bot.send_photo(call.message.chat.id, img1, reply_markup=markup )
+        etap = open('text/1etap.txt', 'rb')
+        bot.send_message(call.message.chat.id, etap)
+        img = open('img/etap1.png', 'rb')
+        bot.send_photo(call.message.chat.id, img, reply_markup=markup )
 
     if call.data == "v1":
         actors[call.message.chat.id][2] += 1
         markup = types.InlineKeyboardMarkup()
         btn1 = types.InlineKeyboardButton(text='Далее', callback_data="game2")
         markup.add(btn1)
-        answer1 = open('text/answer1.txt', 'rb')
-        bot.send_message(call.message.chat.id, answer1)
+        answer = open('text/answer1.txt', 'rb')
+        bot.send_message(call.message.chat.id, answer)
         send_stats(call, markup)
 
     if call.data == "v2":
@@ -59,8 +59,8 @@ def menu (call):
         markup = types.InlineKeyboardMarkup()
         btn1 = types.InlineKeyboardButton(text='Далее', callback_data="game2")
         markup.add(btn1)
-        answer2 = open('text/answer2.txt', 'rb')
-        bot.send_message(call.message.chat.id, answer2)
+        answer = open('text/answer2.txt', 'rb')
+        bot.send_message(call.message.chat.id, answer)
         send_stats(call, markup)
 
     if call.data == "game2":
@@ -68,18 +68,18 @@ def menu (call):
         btn1 = types.InlineKeyboardButton(text='1. Занять у родителей и купить домашку', callback_data="v3")
         btn2 = types.InlineKeyboardButton(text='2. Прийти без домашки ', callback_data="v4")
         markup.add(btn1, btn2)
-        etap2 = open('text/2etap.txt', 'rb')
-        bot.send_message(call.message.chat.id, etap2)
-        file2 = open('img/etap2.png', 'rb')
-        bot.send_photo(call.message.chat.id, file2, reply_markup=markup)
+        etap = open('text/2etap.txt', 'rb')
+        bot.send_message(call.message.chat.id, etap)
+        img = open('img/etap2.png', 'rb')
+        bot.send_photo(call.message.chat.id, img, reply_markup=markup)
 
     if call.data == "v3":
         actors[call.message.chat.id][1] -= 500
         markup = types.InlineKeyboardMarkup()
         btn1 = types.InlineKeyboardButton(text='Далее', callback_data="game3")
         markup.add(btn1)
-        answer3 = open('text/answer3.txt', 'rb')
-        bot.send_message(call.message.chat.id, answer3)
+        answer = open('text/answer3.txt', 'rb')
+        bot.send_message(call.message.chat.id, answer)
         send_stats(call, markup)
 
     if call.data == "v4":
@@ -88,8 +88,8 @@ def menu (call):
         markup = types.InlineKeyboardMarkup()
         btn1 = types.InlineKeyboardButton(text='Далее', callback_data="game3")
         markup.add(btn1)
-        answer4 = open('text/answer4.txt', 'rb')
-        bot.send_message(call.message.chat.id, answer4)
+        answer = open('text/answer4.txt', 'rb')
+        bot.send_message(call.message.chat.id, answer)
         send_stats(call, markup)
 
     if call.data == "game3":
@@ -97,10 +97,10 @@ def menu (call):
         btn1 = types.InlineKeyboardButton(text='1. Вычислить воришку необычным способом', callback_data="v5")
         btn2 = types.InlineKeyboardButton(text='2. Забить на ситуацию, в общаге все общее', callback_data="v6")
         markup.add(btn1, btn2)
-        etap3 = open('text/3etap.txt', 'rb')
-        bot.send_message(call.message.chat.id, etap3)
-        file2 = open('img/etap3.png', 'rb')
-        bot.send_photo(call.message.chat.id, file2, reply_markup=markup)
+        etap = open('text/3etap.txt', 'rb')
+        bot.send_message(call.message.chat.id, etap)
+        img = open('img/etap3.png', 'rb')
+        bot.send_photo(call.message.chat.id, img, reply_markup=markup)
 
     if call.data == "v5":
         actors[call.message.chat.id][1] += 200
@@ -109,8 +109,8 @@ def menu (call):
         markup = types.InlineKeyboardMarkup()
         btn1 = types.InlineKeyboardButton(text='Далее', callback_data="game4")
         markup.add(btn1)
-        answer5 = open('text/answer5.txt', 'rb')
-        bot.send_message(call.message.chat.id, answer5)
+        answer = open('text/answer5.txt', 'rb')
+        bot.send_message(call.message.chat.id, answer)
         send_stats(call, markup)
 
     if call.data == "v6":
@@ -119,25 +119,48 @@ def menu (call):
         markup = types.InlineKeyboardMarkup()
         btn1 = types.InlineKeyboardButton(text='Далее', callback_data="game4")
         markup.add(btn1)
-        answer6 = open('text/answer6.txt', 'rb')
-        bot.send_message(call.message.chat.id, answer6)
+        answer = open('text/answer6.txt', 'rb')
+        bot.send_message(call.message.chat.id, answer)
         send_stats(call, markup)
 
     if call.data == "game4":
         markup = types.InlineKeyboardMarkup()
-        btn1 = types.InlineKeyboardButton(text='1. Пересилить себя и пройти мимо', callback_data="v5")
-        btn2 = types.InlineKeyboardButton(text='2. Закупиться булками и кофейком, но опоздать', callback_data="v6")
+        btn1 = types.InlineKeyboardButton(text='1. Пересилить себя и пройти мимо', callback_data="v7")
+        btn2 = types.InlineKeyboardButton(text='2. Закупиться булками и кофейком, но опоздать', callback_data="v8")
         markup.add(btn1, btn2)
-        etap4 = open('text/4etap.txt', 'rb')
-        bot.send_message(call.message.chat.id, etap4)
-        file2 = open('img/etap4.png', 'rb')
-        bot.send_photo(call.message.chat.id, file2, reply_markup=markup)
+        etap = open('text/4etap.txt', 'rb')
+        bot.send_message(call.message.chat.id, etap)
+        img = open('img/etap4.png', 'rb')
+        bot.send_photo(call.message.chat.id, img, reply_markup=markup)
+
+    if call.data == "v7":
+        actors[call.message.chat.id][1] += 200
+        actors[call.message.chat.id][2] -= 1
+        actors[call.message.chat.id][3] += 10
+        markup = types.InlineKeyboardMarkup()
+        btn1 = types.InlineKeyboardButton(text='Далее', callback_data="game4")
+        markup.add(btn1)
+        answer = open('text/answer7.txt', 'rb')
+        bot.send_message(call.message.chat.id, answer)
+        send_stats(call, markup)
+
+    if call.data == "v8":
+        actors[call.message.chat.id][2] -= 1
+        actors[call.message.chat.id][3] += 15
+        markup = types.InlineKeyboardMarkup()
+        btn1 = types.InlineKeyboardButton(text='Далее', callback_data="game4")
+        markup.add(btn1)
+        answer = open('text/answer8.txt', 'rb')
+        bot.send_message(call.message.chat.id, answer)
+        send_stats(call, markup)
+
+
 
     bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
 
 # git status
 # git add .
-# git commit -m "комит"
+# git commit -m "№ комит"
 # git push
 
 bot.polling(non_stop=True, interval= 0)
